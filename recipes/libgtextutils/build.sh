549b6b9e33cba50828912421ccba15b5ea9ff0b1
yejianbang <yjb_kunpeng@163.com>
2024-04-10 15:18:27 +0800

    linux-aarch64 build for libgtextutils

--- recipes/libgtextutils/build.sh
+++ recipes/libgtextutils/build.sh
@@ -6,6 +6,13 @@ if [ "$(uname)" == "Darwin" ]; then
     # 10.9 seems to be the minimum possible deployment target
     MACOSX_DEPLOYMENT_TARGET=10.9
 fi
+if [ `uname -m` == "aarch64" ]; then
+        rm -rf config.guess config.sub config/config.guess config/config.sub
+        wget "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD" -O config.guess
+        wget "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD" -O config.sub
+        cp config.guess config/config.guess
+        cp config.sub config/config.sub
+fi
 
 ./configure --prefix=$PREFIX
 make
