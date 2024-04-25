c68f22b1877bd626b1dce01e449643af9706ab58
yejianbang <yjb_kunpeng@163.com>
2024-04-25 15:18:23 +0800

    linux-aarch64 build for minimap2

--- recipes/minimap2/build.sh
+++ recipes/minimap2/build.sh
@@ -3,8 +3,13 @@
 mkdir -p $PREFIX/bin $PREFIX/lib $PREFIX/include
 
 export CPATH=${PREFIX}/include
-
-make INCLUDES="-I$PREFIX/include" CFLAGS="-g -Wall -O2 -Wc++-compat -L$PREFIX/lib" minimap2 sdust
+if [ `uname -m` == "aarch64" ]; then
+ sed -i "1c CFLAGS=             -g -Wall -O2 -Wc++-compat -L$PREFIX/lib" Makefile
+ sed -i "3c INCLUDES=-I$PREFIX/include" Makefile
+ make arm_neon=1 aarch64=1 minimap2 sdust
+else
+ make INCLUDES="-I$PREFIX/include" CFLAGS="-g -Wall -O2 -Wc++-compat -L$PREFIX/lib" minimap2 sdust
+fi
 cp minimap2 misc/paftools.js $PREFIX/bin
 cp sdust $PREFIX/bin
 cp libminimap2.a $PREFIX/lib
