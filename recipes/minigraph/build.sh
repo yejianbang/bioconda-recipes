37f1ec5458828f161fba6bda5e71311ec3dc42be
yejianbang <yjb_kunpeng@163.com>
2024-04-26 08:35:39 +0800

    linux-aarch64 build for minigraph

--- recipes/minigraph/build.sh
+++ recipes/minigraph/build.sh
@@ -3,6 +3,9 @@
 mkdir -p $PREFIX/bin
 
 export CPATH=${PREFIX}/include
+if [ `uname -m` == "aarch64" ]; then
+        sed -i "2s/-msse4//g" Makefile
+fi
 
 make  CC="${CC} ${CFLAGS} ${CPPFLAGS} ${LDFLAGS}"  CXX="${CXX} ${CXXFLAGS} ${CPPFLAGS} ${LDFLAGS}"
 cp minigraph $PREFIX/bin
